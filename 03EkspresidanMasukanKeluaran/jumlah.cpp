496e1cb8aee3d40a19d96d35132e7bfb5ab71f3d
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-01-22 16:41:06 +0700

    keluaran

---
+++ 03EkspresidanMasukanKeluaran/jumlah.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+	int a,b;
+
+	printf("masukkan nilai a: ");
+	scanf("%d", &a);
+
+	printf("masukkan nilai b: ");
+	scanf("%d", &b);
+
+	printf("a + b = %d\n", a+b);
+}
