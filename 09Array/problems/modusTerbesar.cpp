4c83c7eacf5fb5116368f5d7d0c804a470823b93
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2023-02-19 05:52:27 +0700

    problems (ACC)

--- 09Array/problems/modusTerbesar.cpp
+++ 09Array/problems/modusTerbesar.cpp
@@ -4,43 +4,34 @@ int main() {
 	int N;
 	scanf("%d", &N);
 	int arr[N];
-	int arrC[N];
-	int higherIndex[N];
-
-	for(int i = 0; i < N; i++) {
-		scanf("%d", &arr[i]);
+	int arrC[1001];
+	
+	for(int i = 0; i < 1001; i++) {
+		arrC[i] = 0;
 	}
 
 	for(int i = 0; i < N; i++) {
-		for(int j = i+1; j < N; j++) {
-			if(arr[i] == arr[j]) {
-				arrC[i] += 1;
+		scanf("%d", &arr[i]);
+		arrC[arr[i]]++;
+	}
+	
+	int higher = -1;
+	
+	for(int i = 0; i < N; i++) {
+		if(arrC[arr[i]] > higher) {
+			higher = arrC[arr[i]];
+		}
+	}
+
+	int finalHigher = -1;
+	
+	for(int i = 0; i < N; i++) {
+		if(arrC[arr[i]] == higher) {
+			if(arr[i] > finalHigher) {
+				finalHigher = arr[i];
 			}
 		}
 	}
 
-	int higherValue = arrC[0];
-
-	for(int i = 0; i < N-1; i++) {
-		if(i == 0) {
-			higherIndex[0] = i;
-		}
-		
-		if(higherValue < arrC[i+1]) {
-			higherIndex[0] = i+1;
-			higherValue = arrC[i+1];
-		}
-	}
-	
-	for(int i = 0; i < N-1; i++) {
-		if(higherValue == arrC[i] && i != higherIndex[0]) {
-			higherIndex[1] = i;
-		}
-	}
-
-	if(arr[higherIndex[0]] < arr[higherIndex[1]]) {
-		printf("%d\n", arr[higherIndex[1]]);
-	} else {
-		printf("%d\n", arr[higherIndex[0]]);
-	}
-} 
+	printf("%d\n", finalHigher);
+}
