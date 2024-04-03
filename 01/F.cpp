267ec9a9dc95748eebbb4c31c3465c8a4d673038
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2024-04-03 08:28:36 +0700

    01 solved

---
+++ 01/F.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cetak luas kandang
+    std::cout << 364 * 79 << std::endl;
+
+    // cetak keliling kandang
+    std::cout << (364 + 79) * 2 << std::endl;
+}
