a53717c21c3a9991866c40a4a4beaf6e80287e91
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2024-04-21 20:40:40 +0700

    D solved

---
+++ 01/D.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a, b;
+	
+	cin >> a >> b;
+	
+	if(a.length() - b.length() != 1) {
+		cout << "Wah, tidak bisa :(" << endl;
+	} else {
+		int c = 0;
+		int j = 0;
+		for(int i = 0; i < a.length(); i++) {
+			if(a[i] != b[j]) {
+				c++;
+			} else {
+				j++;
+			}
+		}
+		
+		if(c == 1) {
+			cout << "Tentu saja bisa!" << endl;
+
+		} else {
+			cout << "Wah, tidak bisa :(" << endl;
+		}
+	}
+}
